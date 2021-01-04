096917c4b0e62cd0a54be675a4d0e013a9533deb
ohtori <ohtori@mail.ru>
2021-01-04 23:45:56 +0400

    more deploy 2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function start() {
         app.get('/get-categories', getCategoriesRouter);
         app.get('/get-category', getCategoryRouter);
         app.get('/images', express.static(path.join(__dirname, 'client', 'public')));
-        if (process.env.MODE === 'production') {
+        if (process.env.MODE === 'production' || process.env.MODE === 'production-unsecure') {
             app.use(express.static(path.join(__dirname, 'client', 'build')));
             app.get('*', (req, res) => {
                 res.status(200).sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -37,7 +37,6 @@ async function start() {
         const PORT = process.env.MODE === 'production'
             ? config.get('serverConfig.HTTPSPort')
             : config.get('serverConfig.devPort');
-        console.log(process.env.MODE);
         if (process.env.MODE === 'production') {
             const httpsOptions = {
                 key: fs.readFileSync("./https-keys/server.key"),
