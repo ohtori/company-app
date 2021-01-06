2a218a2e1095458c178296397aff055f49c5fc7f
ohtori <ohtori@mail.ru>
2021-01-06 12:55:56 +0400

    fix production MODE bugs

--- index.js
+++ index.js
@@ -36,9 +36,8 @@ async function start() {
         }
         if (process.env.MODE === 'production') {
             const httpsOptions = {
-                key: fs.readFileSync("./https-keys/server.key"),
-                cert: fs.readFileSync("./https-keys/server.cert"),
-                ca: fs.readFileSync("./https-keys/server.csr"),
+                key: fs.readFileSync("/etc/letsencrypt/live/ohtori-company.site/privkey.pem"),
+                cert: fs.readFileSync("/etc/letsencrypt/live/ohtori-company.site/fullchain.pem"),
             };
             https.createServer(httpsOptions, app).listen(config.get('serverConfig.HTTPSPort'), () => console.log(`Server started on secure port`));
             await mongoose.connect(config.get('dbConfig.url'), {
@@ -47,7 +46,7 @@ async function start() {
                 useCreateIndex: true
             });
         }
-        if (process.env.MODE === 'production-unsecure') {
+        else if (process.env.MODE === 'production-unsecure') {
             app.listen(config.get('serverConfig.HTTPPort'), () => console.log(`Server started on unsecure port`));
             await mongoose.connect(config.get('dbConfig.url'), {
                 useNewUrlParser: true,
